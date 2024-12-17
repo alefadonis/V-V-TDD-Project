7ebe910ef3b7385d51ac9eed06ab0e78f0d8e403
Jose-Alves-Neto <jose.alves.neto@ccc.ufcg.edu.br>
2024-12-16 21:00:11 -0300

    [SID] Testes de criação Ingresso

---
+++ sistema-de-ingressos/src/test/java/com/tdd/sistema_de_ingressos/IngressoTest.java
@@ -0,0 +1,35 @@
+package com.tdd.sistema_de_ingressos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class IngressoTest {
+    @Test
+    public void testCriarIngressoNormal() {
+        Ingresso ingressoNormal = new Ingresso(TipoIngresso.NORMAL, 10.0);
+
+        assertEquals(10.0, ingressoNormal.getPreco());
+        assertEquals(TipoIngresso.NORMAL, ingresso.getTipo());
+        assertFalse(ingresso.isVendido());
+    }
+
+    @Test
+    public void testCriarIngressoVIP() {
+        Ingresso ingressoVip = new Ingresso(TipoIngresso.VIP, 10.0);
+
+        assertEquals(20.0, ingressoVip.getPreco());
+        assertEquals(TipoIngresso.VIP, ingressoVip.getTipo());
+        assertFalse(ingressoVip.isVendido());
+    }
+
+    @Test
+    public void testCriarIngressoMeia() {
+        Ingresso ingressoMeia = new Ingresso(TipoIngresso.MEIA, 10.0);
+
+        assertEquals(5.0, ingressoMeia.getPreco());
+        assertEquals(TipoIngresso.MEIA, ingressoMeia.getTipo());
+        assertFalse(ingressoMeia.isVendido());
+    }
+
+}
